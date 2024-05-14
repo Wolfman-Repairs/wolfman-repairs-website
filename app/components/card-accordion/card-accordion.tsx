8f9d13e2453086cb196f126fcb48e0e3c2d05626
Chris Racioppo <Gravedigger7789@gmail.com>
2024-05-13 22:30:59 -0400

    made accordion work

---
+++ app/components/card-accordion/card-accordion.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { useState } from "react"
+
+interface IProps {
+    title: string
+    image: string
+    imageAlt: string
+    visibleText: string
+    hiddenText?: string
+}
+
+export default function CardAccordion({ title, image, imageAlt, visibleText, hiddenText }: IProps) {
+    const [expanded, setActive] = useState(false);
+    const toggleExpanded = () => {
+        setActive(!expanded)
+    }
+    return (
+        <div className="md:w-96 flex-1 md:flex-initial h-full rounded overflow-hidden shadow-md shadow-black/50 my-4">
+            <img className="w-full h-44 bg-black dark:bg-neutral-900" src={image} alt={imageAlt} />
+            <div className="p-4 mt-4 ">
+                <div className="font-bold text-xl text-center">{title}</div>
+                <div className="text-base whitespace-pre-line text-center">
+                    {visibleText}
+                </div>
+                <div className={"transition-[max-height] ease-in-out delay-75 text-base whitespace-pre-line text-center " + (expanded ? "max-h-screen" : "invisible max-h-0")}>
+                    <br />
+                    {hiddenText}
+                </div>
+            </div>
+            <button className="w-full text-center bg-gray-200 hover:bg-gray-300 py-2" onClick={toggleExpanded}>
+                <span className="text-sm font-semibold text-gray-700">{expanded ? "- Show Less" : "+ Read More"}</span>
+            </button>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import VideoPlayer from "./components/video-player/video-player";
+import CardAccordion from "./components/card-accordion/card-accordion";
 
 export default function Home() {
   return (
@@ -20,24 +20,21 @@ export default function Home() {
         </div>
       </div>
       <div className="m-4">
-        <div className="">
+        <div className="" id="video">
           <h2 className="text-4xl text-center my-4">Check out the latest video!!</h2>
           <VideoPlayer playlist="UULFUit0DLfO1RQ1LjxVW1rT6Q" />
         </div>
-        <div className="">
+        <div className="" id="about">
           <h2 className="text-4xl text-center my-8">About Wolfman Repairs</h2>
           <div className="flex flex-col md:flex-row justify-center md:justify-evenly md:space-x-6">
-            <div className="md:w-96 flex-1 md:flex-initial h-full rounded overflow-hidden shadow-md shadow-black/50 my-4">
-              <img className="w-full h-44 bg-black dark:bg-neutral-900" src="/test-website-2/next.svg" alt="Sunset in the mountains" />
-              <div className="p-4 mt-4">
-                <div className="font-bold text-xl text-center">Our Story</div>
-                <div className="text-base whitespace-pre-line text-center">
-                  {`
-Welcome to Wolfman Repairs, where passion meets craftsmanship. Founded in late 2022, Wolfman Repairs was born out of a love for retro video games and a desire to breathe new life into classic consoles.
+            <CardAccordion
+              title="Our Story"
+              image="/test-website-2/next.svg"
+              imageAlt="nes motherboard"
+              visibleText={`Welcome to Wolfman Repairs, where passion meets craftsmanship. Founded in late 2022, Wolfman Repairs was born out of a love for retro video games and a desire to breathe new life into classic consoles.
 
-Driven by a lifelong fascination with gaming, our founder embarked on a journey to not only restore vintage gaming systems but to also share the joy of retro gaming with a wider audience. Armed with soldering irons and a deep-seated commitment to quality, Wolfman Repairs began its quest to revive gaming nostalgia one console at a time.
-
-From the dusty attics to the bustling world of eBay, we scoured for forgotten treasures, rescuing them from obscurity and bringing them back to their former glory. Each repair isn't just about fixing a piece of hardware; it's about preserving a piece of gaming history and passing it on to future generations.
+Driven by a lifelong fascination with gaming, our founder embarked on a journey to not only restore vintage gaming systems but to also share the joy of retro gaming with a wider audience. Armed with soldering irons and a deep-seated commitment to quality, Wolfman Repairs began its quest to revive gaming nostalgia one console at a time.`}
+              hiddenText={`From the dusty attics to the bustling world of eBay, we scoured for forgotten treasures, rescuing them from obscurity and bringing them back to their former glory. Each repair isn't just about fixing a piece of hardware; it's about preserving a piece of gaming history and passing it on to future generations.
 
 Along the way, we encountered our fair share of challenges. From stubborn circuit boards to elusive spare parts, every obstacle only fueled our determination to succeed. Through perseverance and ingenuity, we honed our skills and refined our craft, emerging stronger with each conquest.
 
@@ -47,38 +44,22 @@ Since our humble beginnings, Wolfman Repairs has grown into a thriving community
 
 As we look to the future, our vision is clear: to continue expanding our reach, spreading the joy of retro gaming far and wide, and leaving a lasting legacy in the gaming community. Join us on this journey as we embark on new adventures, one pixel at a time.
 
-Thank you for being a part of the Wolfman Repairs family.
-              `}
-                </div>
-              </div>
-              <button className="w-full text-center bg-gray-200 hover:bg-gray-300 py-2">
-                <span className="text-sm font-semibold text-gray-700">- Show Less</span>
-              </button>
-            </div>
-            <div className="md:w-96 flex-1 md:flex-initial h-full rounded overflow-hidden shadow-md shadow-black/50 my-4">
-              <img className="w-full h-44 bg-black dark:bg-neutral-900" src="/test-website-2/next.svg" alt="Sunset in the mountains" />
-              <div className="p-4 mt-4">
-                <div className="font-bold text-xl text-center">Our Team</div>
-                <div className="text-base whitespace-pre-line text-center">
-                  {`
-The driving force behind Wolfman Repairs: the one-man show known simply as the Wolfman. With a passion for retro gaming and a knack for fixing things, the Wolfman is the heart and soul of our operation.
+Thank you for being a part of the Wolfman Repairs family.`} />
 
-Armed with years of experience and an insatiable curiosity, the Wolfman dives headfirst into the world of vintage gaming, armed with soldering iron in hand and a determination to conquer any challenge that comes his way.
+            <CardAccordion
+              title="Our Team"
+              image="/test-website-2/next.svg"
+              imageAlt="n64 chip"
+              visibleText={`The driving force behind Wolfman Repairs: the one-man show known simply as the Wolfman. With a passion for retro gaming and a knack for fixing things, the Wolfman is the heart and soul of our operation.
 
-As the sole member of the Wolfman Repairs team, he wears many hats: technician, curator, marketer, and everything in between. From scouring flea markets for hidden gems to meticulously restoring consoles in his dungeon, the Wolfman's dedication to his craft knows no bounds.
+Armed with years of experience and an insatiable curiosity, the Wolfman dives headfirst into the world of vintage gaming, armed with soldering iron in hand and a determination to conquer any challenge that comes his way.`}
+              hiddenText={`As the sole member of the Wolfman Repairs team, he wears many hats: technician, curator, marketer, and everything in between. From scouring flea markets for hidden gems to meticulously restoring consoles in his dungeon, the Wolfman's dedication to his craft knows no bounds.
 
 But it's not just about fixing consoles; it's about fostering a sense of community and sharing the joy of retro gaming with the world. With each repair, the Wolfman brings a piece of gaming history back to life, sparking nostalgia and creating memories for enthusiasts young and old.
 
 Driven by a commitment to excellence and a passion for preserving gaming heritage, the Wolfman is more than just a repairman; he's a guardian of gaming nostalgia, ensuring that these timeless classics continue to bring joy for years to come.
 
-Join the Wolfman on this journey through the annals of gaming history, as he continues to tinker, repair, and inspire. Together, let's celebrate the magic of retro gaming and keep the spirit of the Wolfman alive.
-              `}
-                </div>
-              </div>
-              <button className="w-full text-center bg-gray-200 hover:bg-gray-300 py-2">
-                <span className="text-sm font-semibold text-gray-700">- Show Less</span>
-              </button>
-            </div>
+Join the Wolfman on this journey through the annals of gaming history, as he continues to tinker, repair, and inspire. Together, let's celebrate the magic of retro gaming and keep the spirit of the Wolfman alive.`} />
           </div>
         </div>
       </div>
