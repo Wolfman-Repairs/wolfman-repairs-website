b4ac4a22188fb26d86b86cdba6ceb4faa1af0aa8
Chris Racioppo <Gravedigger7789@gmail.com>
2024-07-02 00:20:39 -0400

    adding new content card component

---
+++ app/components/content-card/content-card.tsx
@@ -0,0 +1,20 @@
+interface IProps {
+    title: string
+    image: string
+    imageAlt: string
+    text: string
+}
+
+export default function ContentCard({ title, image, imageAlt, text }: IProps) {
+    return (
+        <section className={"flex flex-col my-3 rounded shadow-md shadow-black/50 bg-neutral-800"}>
+            <div style={{ '--image-url': `url(${image})` } as React.CSSProperties}
+                className='bg-center bg-repeat bg-[image:var(--image-url)] bg-blend-multiply bg-neutral-700 shadow-md shadow-black/50'>
+                <div className="text-white font-bold text-4xl text-center m-10">{title}</div>
+            </div>
+            <div className="p-2 md:p-6">
+                <p className="text-white text-base whitespace-pre-line">{text}</p>
+            </div>
+        </section>
+    );
+}
