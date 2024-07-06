fca46720cfc53b9d41133cd3d5317fbc34e4df21
Chris Racioppo <Gravedigger7789@gmail.com>
2024-07-05 23:06:35 -0400

    fixing tags

--- app/components/content-card/content-card.tsx
+++ app/components/content-card/content-card.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 export default function ContentCard({ title, image, imageAlt, text }: IProps) {
     return (
-        <section className={"flex flex-col my-3 rounded shadow-md shadow-black/50 bg-neutral-800"}>
+        <div className={"flex flex-col my-3 rounded shadow-md shadow-black/50 bg-neutral-800"}>
             <div style={{ '--image-url': `url(${image})` } as React.CSSProperties}
                 className='bg-center bg-repeat bg-[image:var(--image-url)] bg-blend-multiply bg-neutral-700 shadow-md shadow-black/50'>
                 <div className="text-white font-bold text-4xl text-center m-10">{title}</div>
@@ -15,6 +15,6 @@ export default function ContentCard({ title, image, imageAlt, text }: IProps) {
             <div className="p-2 md:p-6">
                 <p className="text-white text-base whitespace-pre-line">{text}</p>
             </div>
-        </section>
+        </div>
     );
 }
--- app/components/donation-card/donation-card.tsx
+++ app/components/donation-card/donation-card.tsx
@@ -1,13 +1,13 @@
 export default function DonationCard() {
     return (
-        <div className="flex rounded overflow-hidden shadow-md shadow-black/50">
+        <section className="flex rounded overflow-hidden shadow-md shadow-black/50">
             <div className="flex bg-neutral-800">
                 <img className="object-cover w-full" src="/wolfman-repairs-website/circuit-board.svg" alt="circuit board" />
             </div>
-            <div className="bg-white p-4">
+            <div className="bg-neutral-200 p-4">
                 <div className="mb-4">
-                    <div className="text-gray-900 font-bold text-xl mb-2">Love what we do? Donate To Wolfman Repairs!</div>
-                    <p className="text-gray-700 text-base">Support our mission to revive retro gaming by donating today. Every contribution helps us continue our work of restoring gaming nostalgia and sharing it with enthusiasts worldwide. Join us in preserving gaming history and creating memories for generations to come. Thank you for your support </p>
+                    <div className="text-neutral-900 font-bold text-xl mb-2">Love what we do? Donate To Wolfman Repairs!</div>
+                    <p className="text-neutral-800 text-base">Support our mission to revive retro gaming by donating today. Every contribution helps us continue our work of restoring gaming nostalgia and sharing it with enthusiasts worldwide. Join us in preserving gaming history and creating memories for generations to come. Thank you for your support </p>
                 </div>
 
                 <form className="text-center" action="https://www.paypal.com/donate" method="post" target="_blank">
@@ -15,6 +15,6 @@ export default function DonationCard() {
                     <input className="border-0" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                 </form>
             </div>
-        </div>
+        </section>
     );
 }
