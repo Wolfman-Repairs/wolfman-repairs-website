42e3d00230320cf0138db03370d3e332017a3ea9
Chris Racioppo <Gravedigger7789@gmail.com>
2024-07-07 14:47:06 -0400

    Adding new gallery component

---
+++ app/components/gallery-card/gallery-card.tsx
@@ -0,0 +1,22 @@
+interface IProps {
+    title: string
+    image: string
+    imageAlt: string
+    text: string
+    url?: string
+}
+
+export default function GalleryCard({ title, image, imageAlt, text, url }: IProps) {
+    return (
+        <div style={{ '--image-url': `url(${image})` } as React.CSSProperties}
+            className={"flex w-full rounded shadow-md shadow-black/50 bg-neutral-800 bg-contain bg-no-repeat bg-[95%_50%] bg-[image:var(--image-url)] hover:scale-105 transition-transform"}>
+            <a href={url} target="_blank"
+                className="w-full flex flex-col justify-end">
+                <div className="text-white text-left m-8">
+                    <div className="font-bold text-4xl">{title}</div>
+                    <div className="text-base">{text}</div>
+                </div>
+            </a>
+        </div>
+    );
+}
