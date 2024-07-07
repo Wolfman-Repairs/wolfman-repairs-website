ccac39fbc530bcdba487777a0282d6dfec1a8985
Chris Racioppo <Gravedigger7789@gmail.com>
2024-07-07 17:09:20 -0400

    fixing footer margins

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="p-4 bg-neutral-900 w-full text-neutral-400">
-            <hr className="mb-6 border-neutral-700" />
+        <footer className="mt-8 mb-4 p-4 bg-neutral-900 w-full text-neutral-400">
+            <hr className="mb-8 border-neutral-700" />
             <div className="flex items-center justify-between">
                 <span className="text-sm text-center ">© 2024 Wolfman Repairs™. All Rights Reserved.</span>
                 <div className="flex space-x-2 justify-center">
