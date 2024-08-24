704a1f165b188663da381701a2a5f9c6304ec209
Chris Racioppo <Gravedigger7789@gmail.com>
2024-08-23 20:27:38 -0400

    updating content card margins

--- app/components/content-card/content-card.tsx
+++ app/components/content-card/content-card.tsx
@@ -1,16 +1,16 @@
 interface IProps {
     title: string
-    image: string
-    imageAlt: string
+    image?: string
+    imageAlt?: string
     text: string
 }
 
 export default function ContentCard({ title, image, imageAlt, text }: IProps) {
     return (
         <div className={"flex flex-col w-full rounded-md overflow-hidden shadow-md shadow-black/50 bg-neutral-800"}>
-            <div style={{ '--image-url': `url(${image})` } as React.CSSProperties}
+            <div style={image ? { '--image-url': `url(${image})` } as React.CSSProperties : undefined}
                 className="bg-center bg-repeat bg-[image:var(--image-url)] bg-blend-multiply bg-neutral-700 shadow-md shadow-black/50">
-                <div className="text-white font-bold text-4xl text-center m-10">{title}</div>
+                <div className="text-white font-bold text-4xl text-center m-4 sm:m-8">{title}</div>
             </div>
             <div className="p-2 md:p-6">
                 <p className="text-white text-base whitespace-pre-line">{text}</p>
